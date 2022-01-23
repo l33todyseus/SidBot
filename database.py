26f79420a522f571b4df78501cd156dba5c77029
Ulises Abel Arias García <ulisesariasz@gmail.com>
2022-01-23 14:45:00 -0600

    Se crea módulo para manipular base de datos, SQLITE

---
+++ database.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+conn = sqlite3.connect('sid.db')
+print("Base abierta exitosamente")
+
+def generatedb():
+    conn.execute('''CREATE TABLE PRETRACKER
+        (HORA   TEXT NOT NULL,
+        PRECIO  REAL   NOT NULL  )
+    ''')
